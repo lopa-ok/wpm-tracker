53687971aaeb5ac059221ec87e82797939b37091
philopater <lopa@programmer.net>
2024-08-07 21:20:10 +0300

    Revert "update"
    This reverts commit f73faea184daf317a540b5d10ec2c16ff54c1e6c.

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const accuracyElement = document.getElementById('accuracy');
 
 let startTime;
 let correctChars = 0;
-let totalCharsTyped = 0;
 
 async function fetchRandomWords(count = 10) {
     const response = await fetch(`https://random-word-api.herokuapp.com/word?number=${count}`);
@@ -14,14 +13,11 @@ async function fetchRandomWords(count = 10) {
 }
 
 async function renderNewQuote() {
-    const quote = await fetchRandomWords(10);
+    const quote = await fetchRandomWords();
     quoteDisplayElement.innerText = '';
-    quote.split('').forEach((character, index) => {
+    quote.split('').forEach(character => {
         const characterSpan = document.createElement('span');
-        characterSpan.innerHTML = character === ' ' ? '&nbsp;' : character;
-        if (index === 0) {
-            characterSpan.classList.add('current');
-        }
+        characterSpan.innerText = character;
         quoteDisplayElement.appendChild(characterSpan);
     });
     quoteInputElement.value = null;
@@ -33,7 +29,6 @@ quoteInputElement.addEventListener('input', () => {
     const arrayValue = quoteInputElement.value.split('');
     let correct = true;
     correctChars = 0;
-    totalCharsTyped++;
 
     arrayQuote.forEach((characterSpan, index) => {
         const character = arrayValue[index];
@@ -41,7 +36,7 @@ quoteInputElement.addEventListener('input', () => {
             characterSpan.classList.remove('correct');
             characterSpan.classList.remove('incorrect');
             correct = false;
-        } else if (character === characterSpan.innerText || (character === ' ' && characterSpan.innerHTML === '&nbsp;')) {
+        } else if (character === characterSpan.innerText) {
             characterSpan.classList.add('correct');
             characterSpan.classList.remove('incorrect');
             correctChars++;
@@ -50,20 +45,9 @@ quoteInputElement.addEventListener('input', () => {
             characterSpan.classList.add('incorrect');
             correct = false;
         }
-
-        
-        if (index === arrayValue.length) {
-            characterSpan.classList.add('current');
-        } else {
-            characterSpan.classList.remove('current');
-        }
     });
 
-    
-    if (arrayValue.length === arrayQuote.length && correct) {
-        renderNewQuote();
-    }
-
+    if (correct) renderNewQuote();
     updateAccuracy();
 });
 
@@ -76,16 +60,9 @@ function startTimer() {
 }
 
 function updateAccuracy() {
-    const accuracy = Math.floor((correctChars / totalCharsTyped) * 100);
+    const totalChars = quoteDisplayElement.innerText.length;
+    const accuracy = Math.floor((correctChars / totalChars) * 100);
     accuracyElement.innerText = `Accuracy: ${accuracy}%`;
 }
 
 renderNewQuote();
-
-
-document.addEventListener('click', () => {
-    quoteInputElement.focus();
-});
-
-
-quoteInputElement.focus();
